e2fa01eef42ba2ba52dfe9bce7b2da958c9ee498
s4nji1 <hamzaaitbouhou75@gmail.com>
2025-03-11 02:46:05 +0000

    updates

--- backend/skbt-dashboard/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/skbt-dashboard/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cni')->unique();
             $table->string('phone', 20)->nullable();
             $table->text('address')->nullable();
             $table->rememberToken();
